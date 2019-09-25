223f3a067bdda5349478fee3b2a599ad5a98f042
Alex Zissis <alex@alexzissis.tech>
2019-09-25 17:57:57 -0400

    changed algorithm to start at 0

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,7 +1,7 @@
 public class Fibonacci {
     public static int nonRecursive(int n) {
         // initialise an array of size n
-        int[] sequence = new int[n]; // 1
+        int[] sequence = new int[n+1]; // 1
         int i; // 1
 
         if (n == 0 || n == 1) {
@@ -11,7 +11,7 @@ public class Fibonacci {
         sequence[0] = 1;
         sequence[1] = 1;
         // loop through from 0 to n
-        for(i = 2; i < n; i++) { // n * t, t = 1, running time is n
+        for(i = 2; i <= n; i++) { // n * t, t = 1, running time is n
             // otherwise, sum the previous two values in the array to get the next value
             sequence[i] = sequence[i-1] + sequence[i-2];
         }
@@ -28,7 +28,7 @@ public class Fibonacci {
 
         int fibo1=1, fibo2=1, fibonacci=1;
 
-        for(int i = 2; i < number; i++){
+        for(int i = 2; i <= number; i++){
             fibonacci = fibo1 + fibo2;
             fibo1 = fibo2;
             fibo2 = fibonacci;
@@ -39,7 +39,7 @@ public class Fibonacci {
 
     public static int recursive(int n) {
         // if n is 1 or 0 we return 1 as that's the first 2 values in the Fibonacci sequence
-       if (n == 1 || n == 2) {
+       if (n == 0 || n == 1) {
            return 1;
        } else {
            // if n is more than 1 we recursively call this function again with n-1 and n-2, and this repeats until n is 0 or 1
